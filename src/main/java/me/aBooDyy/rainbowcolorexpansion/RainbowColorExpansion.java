82a2dc2df0aecbb7aac5dd03150df9c0c3fb9ba7
aBo0oDyy <35378106+aBo0oDyy@users.noreply.github.com>
2019-05-06 23:59:50 +0300

    cleared the code (suggest by LemmoTresto), and new version 1.2

--- src/main/java/me/aBooDyy/rainbowcolorexpansion/RainbowColorExpansion.java
+++ src/main/java/me/aBooDyy/rainbowcolorexpansion/RainbowColorExpansion.java
@@ -22,40 +22,23 @@ public class RainbowColorExpansion extends PlaceholderExpansion {
 
     @Override
     public String getVersion() {
-        return "1.1";
+        return "1.2";
     }
 
     @Override
     public String onRequest(OfflinePlayer p, String identifier) {
         identifier = PlaceholderAPI.setBracketPlaceholders(p, identifier);
-        if (identifier.startsWith("custom_")) {
-            text = new StringBuilder();
-            i = 0;
-            String[] args = identifier.replace("custom_", "").split("_", 2);
-            if (args.length <= 1) return null;
-
-            String[] rainbow = args[0].split(",");
-
-            for (char c : args[1].toCharArray()) {
-                if (c == ' ') {
-                    text.append(" ");
-                    continue;
-                }
-
-                text.append(ChatColor.COLOR_CHAR);
-                text.append(rainbow[i]);
-                text.append(c);
-                i++;
-
-                if (i == rainbow.length) {
-                    i = 0;
-                }
-            }
-            return text.toString();
-        }
         text = new StringBuilder();
         String[] rainbow = {"c", "6", "e", "a", "b", "9", "5"};
         i = 0;
+
+        if (identifier.startsWith("custom_")) {
+            String[] args = identifier.replace("custom_", "").split("_", 2);
+            if (args.length <= 1) return null;
+            rainbow = args[0].split(",");
+            identifier = args[1];
+        }
+
         for (char c : identifier.toCharArray()) {
             if (c == ' ') {
                 text.append(" ");
