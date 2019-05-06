742083d5d7c19b4c4254e77f1f5f50b2faf8a70d
aBo0oDyy <35378106+aBo0oDyy@users.noreply.github.com>
2019-05-06 19:55:46 +0300

    Initial commit

---
+++ src/main/java/me/aBooDyy/rainbowcolorexpansion/RainbowColorExpansion.java
@@ -0,0 +1,55 @@
+package me.aBooDyy.rainbowcolorexpansion;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+
+public class RainbowColorExpansion extends PlaceholderExpansion {
+
+    private String[] rainbow = {"c", "6", "e", "a", "b", "9", "5"};
+    private StringBuilder text = new StringBuilder();
+    private int i;
+
+    @Override
+    public String getIdentifier() {
+        return "rainbowcolor";
+    }
+
+    @Override
+    public String getAuthor() {
+        return "aBooDyy";
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0";
+    }
+
+    @Override
+    public String onRequest(OfflinePlayer p, String identifier) {
+        if (identifier.startsWith("custom_")) {
+            String[] args = identifier.replace("custom_", "").split("_", 2);
+            if (args.length <= 1) return null;
+
+            rainbow = args[0].split(",");
+            identifier = args[1];
+        }
+
+        for (char c : identifier.toCharArray()) {
+            if (c == ' ') {
+                text.append(" ");
+                continue;
+            }
+
+            text.append(ChatColor.COLOR_CHAR);
+            text.append(rainbow[i]);
+            text.append(c);
+            i++;
+
+            if (i == rainbow.length) {
+                i = 0;
+            }
+        }
+        return text.toString();
+    }
+}
