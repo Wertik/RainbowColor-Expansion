13b54f9aa77eb92e027ebd758ab1aa6217a715cd
aBo0oDyy <35378106+aBo0oDyy@users.noreply.github.com>
2019-05-06 23:13:29 +0300

    changed a few things, now the placeholders should work properly (Thanks to LemmoTresto for telling me about the issue) and added support for PAPI in the placeholder

--- src/main/java/me/aBooDyy/rainbowcolorexpansion/RainbowColorExpansion.java
+++ src/main/java/me/aBooDyy/rainbowcolorexpansion/RainbowColorExpansion.java
@@ -1,13 +1,13 @@
 package me.aBooDyy.rainbowcolorexpansion;
 
+import me.clip.placeholderapi.PlaceholderAPI;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 
 public class RainbowColorExpansion extends PlaceholderExpansion {
 
-    private String[] rainbow = {"c", "6", "e", "a", "b", "9", "5"};
-    private StringBuilder text = new StringBuilder();
+    private StringBuilder text;
     private int i;
 
     @Override
@@ -27,14 +27,35 @@ public class RainbowColorExpansion extends PlaceholderExpansion {
 
     @Override
     public String onRequest(OfflinePlayer p, String identifier) {
+        identifier = PlaceholderAPI.setBracketPlaceholders(p, identifier);
         if (identifier.startsWith("custom_")) {
+            text = new StringBuilder();
+            i = 0;
             String[] args = identifier.replace("custom_", "").split("_", 2);
             if (args.length <= 1) return null;
 
-            rainbow = args[0].split(",");
-            identifier = args[1];
-        }
+            String[] rainbow = args[0].split(",");
 
+            for (char c : args[1].toCharArray()) {
+                if (c == ' ') {
+                    text.append(" ");
+                    continue;
+                }
+
+                text.append(ChatColor.COLOR_CHAR);
+                text.append(rainbow[i]);
+                text.append(c);
+                i++;
+
+                if (i == rainbow.length) {
+                    i = 0;
+                }
+            }
+            return text.toString();
+        }
+        text = new StringBuilder();
+        String[] rainbow = {"c", "6", "e", "a", "b", "9", "5"};
+        i = 0;
         for (char c : identifier.toCharArray()) {
             if (c == ' ') {
                 text.append(" ");
